d3348b0102795207deedd234ba21bd4fb390b222
Eduardo Camarena Santamaria <eduardo@creditus.mx>
2021-10-20 13:08:06 -0500

    added a -y option to all the installs

--- initial_setup.sh
+++ initial_setup.sh
@@ -3,38 +3,41 @@
 sudo apt-get update
 sudo apt-get upgrade
 
+ssh-keygen -t ed25519
+
 # gnome desktop stuff
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
 gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
 
 # git configurations
-sudo apt-get install git
+sudo apt-get install -y git
 git config --global user.name "eduardo camarena"
 git config --global user.email "lalo.a.camarena@gmail.com"
 git config --global pager.branch false
 git config --global pager.log false
+# this can be used to not use any pager: git config --global pager.core ''
 
 # cool applications
-sudo apt-get install ffmpegthumbnailer
-sudo apt-get install gnome-tweaks
-sudo apt-get install gnome-shell-extensions
+sudo apt-get install -y ffmpegthumbnailer
+sudo apt-get install -y gnome-tweaks
+sudo apt-get install -y gnome-shell-extensions
 
 # install chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get install -y google-chrome-stable
 
 # install brave browser
 sudo apt install apt-transport-https curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
-sudo apt install brave-browser
+sudo apt install -y brave-browser
 
 # programming languages and stuff
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo apt-get install python3 python3-pip
+sudo apt-get install -y python3 python3-pip python3-venv
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 nvm install node # "node" is an alias for the latest version
@@ -47,4 +50,4 @@ sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/p
 rm -f packages.microsoft.gpg
 sudo apt install apt-transport-https
 sudo apt update
-sudo apt install code
+sudo apt install -y code
