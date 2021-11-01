7fba720785e3554f157b27e10a78646622464145
Eduardo Camarena Santamaria <eduardo@creditus.mx>
2021-11-01 13:28:40 -0600

    made the scripts compatible with dnf

--- docker.sh
+++ docker.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
+# the default package manager is apt
+MANAGER="${1:-apt}"
+if $MANAGER == 'apt'; then
+  sudo apt-get remove docker docker-engine docker.io containerd runc
 
-sudo apt-get update
+  sudo apt-get update
 
-sudo apt-get install \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  gnupg \
-  lsb-release
+  sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
+  sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+  sudo apt-get install docker-ce docker-ce-cli containerd.io
+elif $MANAGER == 'dnf'; then
+  sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+  sudo dnf -y install dnf-plugins-core
+  sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+  sudo dnf install docker-ce docker-ce-cli containerd.io
+  sudo systemctl start docker
+fi
 
 sudo groupadd docker
 
--- initial_setup.sh
+++ initial_setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade
+# the default package manager is apt
+MANAGER="${1:-apt}"
+
+sudo $MANAGER update
+sudo $MANAGER upgrade
 
 ssh-keygen -t ed25519
 
@@ -10,7 +13,7 @@ gsettings set org.gnome.shell.app-switcher current-workspace-only true
 gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
 
 # git configurations
-sudo apt-get install -y git
+sudo $MANAGER install -y git
 git config --global user.name "eduardo camarena"
 git config --global user.email "lalo.a.camarena@gmail.com"
 git config --global pager.branch false
@@ -18,59 +21,67 @@ git config --global pager.branch false
 # to use a less pager run the command git config --global pager.<pager_type> "less -FRX"
 
 # cool applications
-sudo apt-get install -y ffmpegthumbnailer
-sudo apt-get install -y gnome-tweaks
-sudo apt-get install -y gnome-shell-extensions
-sudo apt-get install -y psensor
-sudo apt-get install -y vim
-sudo apt-get install -y deluge
+sudo $MANAGER install -y ffmpegthumbnailer
+sudo $MANAGER install -y gnome-tweaks
+sudo $MANAGER install -y gnome-shell-extensions
+sudo $MANAGER install -y psensor
+sudo $MANAGER install -y vim
+sudo $MANAGER install -y deluge
 # run these commands if thumbnails still don't work 
 # sudo apt-get install gstreamer1.0-libav
 # sudo rm -rf $HOME/.cache/thumbnails/
 
-# install chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+if $MANAGER == 'apt'; then
+  # install brave browser
+  sudo apt install apt-transport-https curl
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  sudo apt update
+  sudo apt install -y brave-browser
 
-# install brave browser
-sudo apt install apt-transport-https curl
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install -y brave-browser
+  # install vscode
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  rm -f packages.microsoft.gpg
+  sudo apt install apt-transport-https
+  sudo apt update
+  sudo apt install -y code
+
+  # install steam
+  sudo apt-get update
+  sudo apt-get upgrade
+  sudo add-apt-repository multiverse
+  sudo apt-get install -y steam
+
+  # install lutris
+  sudo add-apt-repository ppa:lutris-team/lutris
+  sudo apt update
+  sudo apt install lutris
+elif $MANAGER == 'dnf'; then
+  #install brave browser
+  sudo dnf install dnf-plugins-core
+  sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+  sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+  sudo dnf install brave-browser
+
+  #install vscode
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+  dnf check-update
+  sudo dnf install code
+
+  #install steam
+  sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+  sudo dnf install steam
+
+  #install lutris
+  sudo dnf install lutris
+fi
 
 # programming languages and stuff
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo apt-get install -y python3 python3-pip python3-venv
+sudo $MANAGER install -y python3 python3-pip python3-venv
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-nvm install node # "node" is an alias for the latest version
-
-
-# install vscode
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install -y code
-
-# install lutris
-sudo dpkg --add-architecture i386
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key
-sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
-sudo apt-get update
-sudo apt-get install -y --install-recommends winehq-stable
-sudo add-apt-repository ppa:lutris-team/lutris
-sudo apt-get update
-sudo apt-get -y install lutris
-
-# install steam
-sudo apt-get update
-sudo apt-get upgrade
-sudo add-apt-repository multiverse
-sudo apt-get install -y steam
+nvm install --lts
